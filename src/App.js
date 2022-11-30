807fdae28fb2c471762e92ee44bda3a9c7d49b56
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2022-11-30 16:59:41 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState([
+    '남자코트 추천',
+    '강남 우동맛집',
+    '파이썬독학',
+  ]);
+  const [up, setUp] = useState(0);
+  function upHandler() {
+    let count = up + 1;
+    setUp(count);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>리액트 블로그</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {title[0]}{' '}
+          <span style={{ userSelect: 'none' }} onClick={upHandler}>
+            👍좋아요
+          </span>{' '}
+          : {up}
+        </h4>
+        <p>11월 30일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>11월 30일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>11월 30일 발행</p>
+      </div>
+      <Modal />
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
